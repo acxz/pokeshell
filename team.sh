9831ce90bcf3b855fc8768408df7359ddb875252
acxz <17132214+acxz@users.noreply.github.com>
2022-02-27 14:14:23 -0500

    add upscaling,distort,sharpening

--- team.sh
+++ team.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# FIXME: catimg autoscales on width not height (how to autoscale on the smaller one)
+
 echo "Starting Team Display"
 
-team_entries=(1 25 97 145 143 148)
+team_entries=(1 4 7 25 39 37)
+# case for autoscaling based on width and can't see full height
+#team_entries=(487)
 
 for _i in "${team_entries[@]}"; do
 
@@ -11,6 +15,15 @@ for _i in "${team_entries[@]}"; do
 
     # Normalize images (have same bottom) by default images are centered
     convert "$_i.png" -trim "$_i.png"
+
+    # Upscale images for higher resolution
+    convert "$_i.png" -magnify -magnify -magnify "$_i.png"
+
+    # Distort handles removing white pixel border on newer gens>5
+    convert "$_i.png" +distort SRT '1,0' "$_i.png"
+
+    # Sharpen higher res image to reduce blur
+    convert "$_i.png" -unsharp 0x6+0.5+0 "$_i.png"
 
     # Flip to level the base of each image
     convert -flip "$_i.png" "$_i.png"
