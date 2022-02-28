df414090fbfdaa73e562685e3a90f5404cff36d8
acxz <17132214+acxz@users.noreply.github.com>
2022-02-27 23:53:05 -0500

    ight

--- agteam.sh
+++ agteam.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-# gif are not the same length...
-# down/upsample gifs to be a uniform length
-# use average length of specified pokes as the uniform length
-
-# Other option:
-# display multiple gifs at the same time
-# upstream catimg issue?
-
-# first one has to be the shortest time gif to stop pokemon from disappearing
-
 echo "Starting Team Display"
 
-team_entries=(7 4 1 25)
-# team_entries=("bulbasaur" "charmander" "squirtle" "pikachu" "giratina" "zubat" "shaymin")
-# team_entries=("squirtle" "charmander" "bulbasaur" "pikachu")
+#team_entries=(7 4 1 25)
+#team_entries=("bulbasaur" "charmander" "squirtle" "pikachu" "giratina" "zubat" "shaymin")
+#team_entries=("bulbasaur" "charmander" "squirtle" "pikachu")
+team_entries=("zapdos" "articuno" "moltres" "lugia")
+#team_entries=("lugia" "squirtle")
+#team_entries=("giratina" "squirtle")
+#team_entries=("squirtle" "pikachu")
 
 _width=0
-_loop_num=0
+min_frame=100
 
 for _i in "${team_entries[@]}"; do
-
     # Curl images
-    curl -sL "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/$_i.gif" -o "$_i.gif"
-    # curl -sL "https://projectpokemon.org/images/normal-sprite/$_i.gif" -o "$_i.gif"
+    #curl -sL "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/$_i.gif" -o "${_i}.gif"
+    curl -sL "https://projectpokemon.org/images/normal-sprite/$_i.gif" -o "${_i}.gif"
 
     # Flip to level the base of each image
-    convert -flip "$_i.gif" "$_i.gif"
+    convert -flip "${_i}.gif" "$_i.gif"
 
     # Get size
     _prev_width=${_width}
@@ -38,6 +31,84 @@ for _i in "${team_entries[@]}"; do
     sum_widths+=("${_sum_width}")
     (( _height > max_height )) && max_height=${_height}
 
+    _frame=$(identify "$_i.gif" | wc -l)
+    frames+=("${_frame}")
+    (( _frame < min_frame )) && min_frame=${_frame}
+done
+
+_loop_num=0
+for _i in "${team_entries[@]}"; do
+    _frame=${frames[$_loop_num]}
+
+    # No need to sync with the smallest frame gif
+    if [ "${_frame}" != "${min_frame}" ]; then
+
+        # Ceiling division
+        _div=$(((_frame + min_frame) / min_frame))
+        idx_del_str=""
+
+        # Use another interval for missing frames
+        missing_frames=$((min_frame - (_frame + _div) / _div))
+        # Division by zero guard
+        if [ "$missing_frames" != 0 ]; then
+            missing_frames_div=$(((_frame + missing_frames) / missing_frames))
+        fi
+
+        # Loop through frames to see which ones to delete
+        prev_min_frame_num=-1
+        prev_missing_frame_num=-1
+        added_missing_frame=0
+        for (( _frame_num=0; _frame_num<=_frame; _frame_num++ )); do
+            min_frame_num=$((_frame_num / _div))
+            # Division by zero guard
+            if [ "$missing_frames" != 0 ]; then
+                missing_frame_num=$((_frame_num / missing_frames_div))
+
+                # Reset missing frame counter
+                if [ "$missing_frame_num" != "$prev_missing_frame_num" ]; then
+                    added_missing_frame=0
+                fi
+            else
+                missing_frame_num="${prev_missing_frame_num}"
+            fi
+
+            # Check if frame has been added for current min frame interval
+            if [ "$min_frame_num" == "$prev_min_frame_num" ] && \
+               [ "$added_missing_frame" == 1 ]; then
+                # Check if frame has been added for current missing frame interval
+                idx_del_str="$idx_del_str$_frame_num,"
+            fi
+
+            # Check if frame has been added for current missing frame interval
+            if [ "$missing_frame_num" == "$prev_missing_frame_num" ]; then
+                added_missing_frame=1
+            fi
+
+            prev_min_frame_num="${min_frame_num}"
+            prev_missing_frame_num="${missing_frame_num}"
+        done
+
+        # Delete extra frames
+        convert "$_i.gif" -delete "$idx_del_str" "$_i.gif"
+
+        # Pad gif with duplicates for missing frames
+        still_missing=$((min_frame - $(identify "$_i.gif" | wc -l)))
+        echo frame$_frame
+        for (( _missing_num=0; _missing_num<still_missing; _missing_num++ )); do
+            # TODO: dup evenly
+            idx=$(( _missing_num * (_frame - 1) / 2))
+            echo idx$idx
+            #convert "$_i.gif[$idx]" "$_i.gif" "$_i.gif"
+            convert "$_i.gif[0]" "$_i.gif" "$_i.gif"
+        done
+
+        fi
+    _loop_num=$((_loop_num + 1))
+done
+
+_loop_num=0
+for _i in "${team_entries[@]}"; do
+    # FIXME: figure out how to add them all at once instead of one at a time
     # Create first combined gif
     if [[ ${_loop_num} == 1 ]]; then
         convert "${team_entries[0]}.gif" -repage "${sum_widths[${_loop_num}]}"x"${max_height}" -coalesce \
@@ -55,7 +126,6 @@ for _i in "${team_entries[@]}"; do
     fi
 
     _loop_num=$((_loop_num + 1))
-
 done
 
 # Flip image back
@@ -63,7 +133,6 @@ convert -flip t.gif t.gif
 
 # Display
 timg t.gif # works with genV 2d and gen8 3d sprites
-# catimg t.gif # works with gen8 3d sprites
 
 # Cleanup
 for _i in "${team_entries[@]}"; do
