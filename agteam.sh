5cbdd34c7189b05ddfae19606dc2a1049202fd7a
acxz <17132214+acxz@users.noreply.github.com>
2022-02-28 17:54:42 -0500

    less math, better code

--- agteam.sh
+++ agteam.sh
@@ -6,7 +6,7 @@ echo "Starting Team Display"
 #team_entries=("bulbasaur" "charmander" "squirtle" "pikachu" "giratina" "zubat" "shaymin")
 team_entries=("bulbasaur" "charmander" "squirtle" "charizard-megax")
 #team_entries=("munchlax" "jigglypuff" "rayquaza" "gyarados")
-#team_entries=("zapdos" "articuno" "moltres" "lugia" "zubat" "yveltal")
+team_entries=("zapdos" "articuno" "moltres" "lugia" "zubat" "yveltal")
 #team_entries=("lugia" "squirtle")
 #team_entries=("giratina" "squirtle")
 #team_entries=("squirtle" "pikachu")
@@ -16,6 +16,7 @@ _type=normal
 
 _width=0
 min_frame=100
+frame_sum=0
 
 for _i in "${team_entries[@]}"; do
     # Curl images
@@ -38,77 +39,43 @@ for _i in "${team_entries[@]}"; do
     _frame=$(identify "$_i.gif" | wc -l)
     frames+=("${_frame}")
     (( _frame < min_frame )) && min_frame=${_frame}
+    frame_sum=$(( frame_sum + _frame ))
 done
+
+# TODO logic to pick avg/min/in between
+avg_frame=$(( (frame_sum + ${#team_entries[@]} / 2) / ${#team_entries[@]} ))
+echo "$min_frame"
+echo "$avg_frame"
+specified_frame=$min_frame
 
 _loop_num=0
 for _i in "${team_entries[@]}"; do
     _frame=${frames[$_loop_num]}
 
-    # No need to sync with the smallest frame gif
-    if [ "${_frame}" != "${min_frame}" ]; then
-
-        frame_diff=$(( _frame - min_frame ))
-        _div=$((_frame / min_frame))
-        idx_del_str=""
-
-        # Use another interval for extra frames
-        extra_frames=$((_frame / _div - min_frame ))
-        # Division by zero guard
-        if [ "$extra_frames" != 0 ]; then
-            extra_frames_div=$((_frame / extra_frames))
-        fi
-
-        # Loop through diff frames to see which ones to delete
-        prev_extra_frame_num=-1
-        added_extra_frame=0
-        for (( min_frame_num=0; min_frame_num<=frame_diff; min_frame_num++ )); do
-            _frame_num=$(( min_frame_num * _frame / frame_diff ))
-            # Division by zero guard
-            if [ "$extra_frames" != 0 ]; then
-                extra_frame_num=$((_frame_num / extra_frames_div))
-
-                # Reset extra frame counter
-                if [ "$extra_frame_num" != "$prev_extra_frame_num" ]; then
-                    added_extra_frame=0
-                fi
-            else
-                extra_frame_num="${prev_extra_frame_num}"
-            fi
-
-            # Remove frame for current min frame interval
-            if [ "$added_extra_frame" == 1 ]; then
-                # Check if frame has been added for current extra frame interval
-                idx_del_str="$idx_del_str$_frame_num,"
-            fi
-
-            # Check if frame has been added for current extra frame interval
-            if [ "$extra_frame_num" == "$prev_extra_frame_num" ]; then
-                added_extra_frame=1
-            fi
-
-            prev_extra_frame_num="${extra_frame_num}"
-        done
-
-        # Delete frames
-        if [ "$idx_del_str" != "" ]; then
-            convert "$_i.gif" -delete "$idx_del_str" "$_i.gif"
-        fi
-
+    if [ "$_frame" -gt "$specified_frame" ]; then
         # Delete extra frames from gif
-        new_frame=$(identify "$_i.gif" | wc -l)
-        still_extra=$(( new_frame - min_frame ))
+        extra_frames=$(( _frame - specified_frame ))
         extra_idx_del_str=""
-        for (( _extra_num=0; _extra_num<still_extra; _extra_num++ )); do
-            idx=$(( _extra_num * new_frame / still_extra))
+
+        for (( _extra_num=0; _extra_num<extra_frames; _extra_num++ )); do
+            idx=$(( _extra_num * _frame / extra_frames ))
             extra_idx_del_str="$extra_idx_del_str$idx,"
         done
 
-        # Delete extra frames
-        if [ "$extra_idx_del_str" != "" ]; then
-            convert "$_i.gif" -delete "$extra_idx_del_str" "$_i.gif"
-        fi
+        convert "$_i.gif" -delete "$extra_idx_del_str" "$_i.gif"
+    else
+        # Insert extra frames into gif
+        extra_frames=$(( specified_frame - _frame ))
 
-        fi
+        for (( _extra_num=0; _extra_num<extra_frames; _extra_num++ )); do
+            idx=$(( _extra_num * specified_frame / extra_frames ))
+            convert "$_i.gif" "$_i.gif[$idx]" -insert $idx "$_i.gif"
+        done
+
+    fi
+
+    identify "$_i.gif" | wc -l
+
     _loop_num=$((_loop_num + 1))
 done
 
