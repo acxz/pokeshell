4fae8fd010b18f927592fd5dc3e9baa07a9348b5
acxz <17132214+acxz@users.noreply.github.com>
2022-03-02 22:36:00 -0500

    call coalesce in one go

--- agteam.sh
+++ agteam.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Starting Team Display"
 
@@ -6,7 +6,7 @@ echo "Starting Team Display"
 #team_entries=("bulbasaur" "charmander" "squirtle" "pikachu" "giratina" "zubat" "shaymin")
 team_entries=("bulbasaur" "charmander" "squirtle" "charizard-megax")
 #team_entries=("munchlax" "jigglypuff" "rayquaza" "gyarados")
-#team_entries=("zapdos" "articuno" "moltres" "lugia" "zubat" "yveltal")
+team_entries=("zapdos" "articuno" "moltres" "lugia" "charizard-megay" "yveltal")
 #team_entries=("lugia" "squirtle")
 #team_entries=("giratina" "squirtle")
 #team_entries=("giratina" "squirtle")
@@ -19,20 +19,18 @@ _width=0
 min_frame=100
 
 for _i in "${team_entries[@]}"; do
-    # Curl images
+    # Curl gifs
     #curl -sL "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/$_i.gif" -o "${_i}.gif"
     curl -sL "https://projectpokemon.org/images/$_type-sprite/$_i.gif" -o "${_i}.gif"
 
-    # Flip to level the base of each image
+    # Flip to level the base of each gif
     convert -flip "${_i}.gif" "$_i.gif"
 
     # Get size
-    _prev_width=${_width}
-    _prev_sum_width=$((_prev_sum_width + _width))
     _width=$(convert "$_i.gif[0]" -ping -format "%w" info:)
     _height=$(convert "$_i.gif[0]" -ping -format "%h" info:)
 
-    _sum_width=$((_prev_sum_width + _width))
+    _sum_width=$((_sum_width + _width))
     sum_widths+=("${_sum_width}")
     (( _height > max_height )) && max_height=${_height}
 
@@ -42,9 +40,9 @@ for _i in "${team_entries[@]}"; do
 done
 
 specified_frame=$min_frame
+coalesce_str="convert ${team_entries[0]}.gif"
 
-_loop_num=0
-for _i in "${team_entries[@]}"; do
+for _loop_num in "${!team_entries[@]}"; do
     _frame=${frames[$_loop_num]}
 
     if [ "$_frame" -gt "$specified_frame" ]; then
@@ -57,41 +55,29 @@ for _i in "${team_entries[@]}"; do
             extra_idx_del_str="$extra_idx_del_str$idx,"
         done
 
-        convert "$_i.gif" -delete "$extra_idx_del_str" "$_i.gif"
+        convert "${team_entries[_loop_num]}.gif" -delete "$extra_idx_del_str" "${team_entries[_loop_num]}.gif"
     else
         # Insert extra frames into gif
         extra_frames=$(( specified_frame - _frame ))
 
         for (( _extra_num=0; _extra_num<extra_frames; _extra_num++ )); do
             idx=$(( _extra_num * _frame / extra_frames + _extra_num ))
-            convert "$_i.gif" "$_i.gif[$idx]" -insert $idx "$_i.gif"
+            convert "${team_entries[_loop_num]}.gif" "${team_entries[_loop_num]}.gif[$idx]" -insert $idx "${team_entries[_loop_num]}.gif"
         done
-
     fi
 
-    _loop_num=$((_loop_num + 1))
+    if [[ ${_loop_num} -gt 0 ]]; then
+        coalesce_str="${coalesce_str} \
+            -repage ${sum_widths[-1]}x${max_height} -coalesce \
+            null: \( ${team_entries[_loop_num]}.gif -coalesce \) \
+            -geometry +${sum_widths[$((_loop_num - 1))]}+0 \
+            -layers Composite\
+        "
+    fi
 done
 
-_loop_num=0
-for _i in "${team_entries[@]}"; do
-    # Create first combined gif
-    if [[ ${_loop_num} == 1 ]]; then
-        convert "${team_entries[0]}.gif" -repage "${sum_widths[${_loop_num}]}"x"${max_height}" -coalesce \
-                 null: \( "$_i.gif" -coalesce \) \
-                 -geometry +"${sum_widths[$((_loop_num - 1))]}"+0 \
-                 -layers Composite t.gif
-    fi
-
-    # Keep on adding gifs
-    if [[ ${_loop_num} -gt 1 ]]; then
-        convert t.gif -repage "${sum_widths[${_loop_num}]}"x"${max_height}" -coalesce \
-                 null: \( "$_i.gif" -coalesce \) \
-                 -geometry +"${sum_widths[$((_loop_num - 1))]}"+0 \
-                 -layers Composite t.gif
-    fi
-
-    _loop_num=$((_loop_num + 1))
-done
+coalesce_str="${coalesce_str} t.gif"
+eval "$coalesce_str"
 
 # Flip image back
 convert -flip t.gif t.gif
@@ -102,6 +88,7 @@ for _i in "${team_entries[@]}"; do
 done
 
 # Display
-timg t.gif
+#timg t.gif
+catimg t.gif
 
 rm -f t.gif
