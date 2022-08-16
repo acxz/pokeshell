9ed5a6c2e70f538f297a8730d7ccb87a4fa2dd28
acxz <17132214+acxz@users.noreply.github.com>
2022-08-16 04:45:24 -0400

    clean up install/uninstall scripts

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
 
-mkdir -p /usr/local/bin
-cp -v pokeshell /usr/local/bin
-
-mkdir -p /usr/local/share/bash-completion/completions
-cp -v pokeshell-completion.bash /usr/local/share/bash-completion/completions/pokeshell
-
-mkdir -p /usr/local/share/zsh/site-functions
-cp -v pokeshell-completion.bash /usr/local/share/zsh/site-functions/_pokeshell
+mkdir -pv /usr/local
+cp -rv bin /usr/local
+cp -rv share /usr/local
--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,7 @@ rm -v /usr/local/bin/pokeshell
 rm -v /usr/local/share/bash-completion/completions/pokeshell
 rm -v /usr/local/share/zsh/site-functions/_pokeshell
 
+rmdir -v /usr/local/bin
 rmdir -v /usr/local/share/bash-completion/completions
 rmdir -v /usr/local/share/bash-completion
 rmdir -v /usr/local/share/zsh/site-functions
