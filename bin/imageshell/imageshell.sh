e1533b55c6640f4ae417cf3067d707f4f99a4752
acxz <17132214+acxz@users.noreply.github.com>
2024-10-21 01:06:37 -0400

    move trim into another magick commond for speed

--- bin/imageshell/imageshell.sh
+++ bin/imageshell/imageshell.sh
@@ -23,11 +23,6 @@ function imgshl_stitch_images () {
     scale=${2:?}
     cache_dir=${3:?}
 
-    for _i in "${_images[@]}"; do
-        # Ground images (for centered images)
-        magick "$_i" -trim "$_i"
-    done
-
     if [ ${#_images[@]} != 1 ]; then
         if [ $scale == 1 ]; then
             # Get scale height to resize all images to
@@ -44,13 +39,13 @@ function imgshl_stitch_images () {
             scale_height=$max_height
             (( max_height > terminal_height )) && scale_height=$terminal_height
 
-            # Scale and stitch them
-            magick "${_images[@]}" -scale x"${scale_height}" +append "$cache_dir/t.tiff"
+            # Trim, scale, and stitch them
+            magick "${_images[@]}" -trim -scale x"${scale_height}" +append "$cache_dir/t.tiff"
         else
-            # Stitch them
+            # Trim and stitch them
             # PNG: hack for pokeshell, because chafa doesn't like pokeshell tiff images
             # at small image sizes png vs tiff doesn't cause any noticeable slowdown anyway
-            magick -background 'rgba(0, 0, 0, 0' "${_images[@]}" -gravity South +append PNG:"$cache_dir/t.tiff"
+            magick -background 'rgba(0, 0, 0, 0' "${_images[@]}" -trim -gravity South +append PNG:"$cache_dir/t.tiff"
         fi
         display_file="$cache_dir/t.tiff"
     else
