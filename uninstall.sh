df9b7c6d1538fbd06517adc69b73f04a72f92f90
acxz <17132214+acxz@users.noreply.github.com>
2024-10-26 09:30:34 -0400

    remove cache upon uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,3 +9,5 @@ rm -v "${1:-${install_dir}}share/bash-completion/completions/pokeshell"
 rmdir -v "${1:-${install_dir}}bin"
 rmdir -v "${1:-${install_dir}}share/bash-completion/completions"
 rmdir -v "${1:-${install_dir}}share/bash-completion"
+
+rm -rv "${HOME}/.cache/pokeshell" | grep -v "removed '"
